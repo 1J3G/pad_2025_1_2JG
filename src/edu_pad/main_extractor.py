2955d52f116dbc11419fd80a972296f53fdd220e
J3G75 <juan.gonzalez@est.iudigital.edu.co>
2025-06-08 11:19:15 -0500

    ruta csv

--- src/edu_pad/main_extractor.py
+++ src/edu_pad/main_extractor.py
@@ -7,7 +7,7 @@ def main():
     dw = DataWeb()
     df=pd.DataFrame()
     df=dw.obtenerDatos() # capa 2
-    df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False)
+    df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False,mode="a")
 
 
 if __name__ == "__main__":
